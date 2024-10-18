c13bc4b9e9b076e4f84bcab7e38d889b8a1a9e0f
Rio Anugrah Adam Saputra <rioanugrah@Rios-MacBook-Air.local>
2024-10-18 22:25:06 +0700

    Eagle Informatika

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @viteReactRefresh
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @viteReactRefresh --}}
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
 <body>
     <div id="app">
