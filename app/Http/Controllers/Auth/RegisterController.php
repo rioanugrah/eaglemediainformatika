c2e23f051218e55e92d09f65c07d90ecf2438198
Rio Anugrah Adam Saputra <rioanugrah@Rios-MacBook-Air.local>
2024-10-19 19:26:51 +0700

    Eagle Informatika

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -78,7 +78,7 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
 
-        $user->assignRole(1);
+        $user->assignRole(2);
         return $user;
     }
 }
